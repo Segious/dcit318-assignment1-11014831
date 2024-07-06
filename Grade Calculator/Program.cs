87fde4cce87d74dd917ae5b28948cc08436822d0
Segious Kotoku <skotoku001@st.ug.edu.gh>
2024-07-06 00:22:01 +0000

    Completed grade calculator

---
+++ Grade Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a numerical grade between 0 and 100: ");
+           double grade = Convert.ToDouble(Console.ReadLine());
+            
+                if (grade >= 90 && grade <= 100)
+                {
+                    Console.WriteLine("Grade: A");
+                }
+                else if (grade >= 80 && grade <= 89)
+                {
+                    Console.WriteLine("Grade: B");
+                }
+                else if (grade >= 70 && grade <= 79)
+                {
+                    Console.WriteLine("Grade: C");
+                }
+                else if (grade >= 60 && grade <= 69)
+                {
+                    Console.WriteLine("Grade: D");
+                }
+                else if (grade <= 60 && grade >= 0)
+            {
+                    Console.WriteLine("Grade: F");
+                }
+            
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid numerical grade.");
+            }
+        }
+    }
+}
